da9df7866c82b06b60e0a42182b38b972627586c
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-03 14:01:04 -0700

    Added routes

--- bin/app.js
+++ bin/app.js
@@ -1,27 +1,59 @@
 'strict'
 
-const express=require('express');
-const bodyParser=require('body-parser');
-const request=require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
-const app=express();
+const app = express();
 
-app.set('port',(process.env.PORT||5000));
+app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.send("Hello World from chatbot");
 })
 
-app.get('/webhook',function(req,res){
-    if(req.query['hub.verify_token']==="bluelime"){
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === "bluelime") {
         res.send(req.query['hub.challenge']);
     }
     res.send("The token is wrong")
 })
 
-app.listen(app.get('port'),function(){
+let token="EAATy5ujsRDABAHtB4MbI47LtQq36t2g9IQNVaJwNXBvOxh7f8Bmt2BnWZC0DvRZARfkrpIBcxuDSZB43uYO9w9zwK9eIcoeEeZATyWCGmKdgqg9MEyCSC5ZC93AHon5nLeAhksARE0h3ijXjAf50y0s1CaP2DRwxKITCx6lWSRMSUM0wKHKsn"
+
+app.post('/webhook', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i];
+        let sender = event.sender.id;
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            sendText(sender, "Text echo: " + text.substring(0, 100));
+        }
+    }
+})
+
+function sendText(sender, text) {
+    let messageData = { text: text };
+    request({
+        url: "http://graph.facebook.com/v9.0/me/messages",
+        qs: { access_token: token },
+        method: "POST",
+        json: {
+            recipient: { id: sender },
+            message: messageData
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Sending error messages')
+        } else if (response.body.error) {
+            console.log("response body error messages")
+        }
+    })
+}
+app.listen(app.get('port'), function () {
     console.log("Running app")
 })
